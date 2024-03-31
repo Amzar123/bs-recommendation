30c2431d78e140e9d4b0e875b6a61cf4360300cb
Amzar123 <aji.muhammad.zapar080500@gmail.com>
2024-03-31 17:52:53 +0700

    update some file

--- src/model/recommendation.py
+++ src/model/recommendation.py
@@ -5,11 +5,13 @@ class Recommendation(Util.get_db().Model):
 
     id = Util.get_db().Column(Util.get_db().Integer, primary_key=True)
     recommendation = Util.get_db().Column(Util.get_db().String(50))
+    student_id = Util.get_db().Column(Util.get_db().Integer, Util.get_db().ForeignKey('students.id'))
     created_at = Util.get_db().Column(Util.get_db().DateTime)
     updated_at = Util.get_db().Column(Util.get_db().DateTime)
 
-    def __init__(self, recommendation, created_at, updated_at):
+    def __init__(self, recommendation, student_id, created_at, updated_at):
         self.recommendation = recommendation
+        self.student_id = student_id
         self.created_at = created_at
         self.updated_at = updated_at
 
