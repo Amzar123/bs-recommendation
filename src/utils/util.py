f75791e34a21d92b8734154899d086a8aa829cbf
Amzar123 <aji.muhammad.zapar080500@gmail.com>
2024-05-01 08:34:12 +0700

    apply pylint and autopep8

--- src/utils/util.py
+++ src/utils/util.py
@@ -1,14 +1,33 @@
+"""
+It demonstrates the usage of Flask along with SQLAlchemy and Flask-Migrate for database migrations.
+"""
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+
 class Util:
+    """
+    Utility class for managing Flask application, database connection, and migration.
+
+    This class provides static methods to get the Flask application instance, database connection,
+    and Migrate object for the application.
+    """
     _app = None
     _db = None
     _migrate = None
 
     @staticmethod
     def get_app():
+        """
+        Returns the Flask application instance.
+
+        If the application instance does not exist, 
+        it creates a new instance and configures the database URI.
+
+        Returns:
+            Flask: The Flask application instance.
+        """
         if Util._app is None:
             Util._app = Flask(__name__)
             Util._app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/database_name'
@@ -16,12 +35,27 @@ class Util:
 
     @staticmethod
     def get_db():
+        """
+        Get the database connection.
+
+        Returns:
+            SQLAlchemy: The database connection object.
+        """
         if Util._db is None:
             Util._db = SQLAlchemy(Util.get_app())
         return Util._db
 
     @staticmethod
     def get_migrate():
+        """
+        Returns the Migrate object for the application.
+
+        If the Migrate object is not yet created, it creates a new one using the
+        get_app() and get_db() methods.
+
+        Returns:
+            Migrate: The Migrate object for the application.
+        """
         if Util._migrate is None:
             Util._migrate = Migrate(Util.get_app(), Util.get_db())
         return Util._migrate
